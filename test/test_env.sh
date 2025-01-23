d2ccc8a59c43877e58f66bf4f5723abf6ae4f4f5
George Gevoian <george@gevoian.com>
2025-01-23 12:04:10 -0800

    Update test_env.sh

--- test/test_env.sh
+++ test/test_env.sh
@@ -3,10 +3,10 @@
 export GRIST_SESSION_COOKIE="grist_test_cookie"
 export LANGUAGE="en_US"
 export SE_BROWSER="chrome"
-export SE_BROWSER_VERSION="131"
+export SE_BROWSER_VERSION="128"
 export SE_DRIVER="chrome-driver"
-export SE_DRIVER_VERSION="131.0.6778.69"
+export SE_DRIVER_VERSION="128.0.6613.119"
 export TEST_CLEAN_DATABASE="true"
 export TEST_SUPPORT_API_KEY="api_key_for_support"
-
+export MOCHA_WEBDRIVER_ARGS="--no-sandbox"
 exec "$@"
